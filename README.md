13ff35adc3c1b0dc8e71f093f30594cc40728f98
David Jones <david.jones@madwiremedia.com>
2020-10-17 15:21:31 -0600

    initialize

---
+++ README.md
@@ -0,0 +1,1 @@
+# bhyve-api
---
+++ config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  auth: {
+    email: 'do@change.me',
+    password: 'changeme'
+  },
+  device: 1, // Of index
+  zone: 1,
+  runTime: 15,  // Seconds
+  delayTime: 60, // Seconds
+}
